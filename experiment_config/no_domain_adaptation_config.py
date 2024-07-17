b0683224d7cb6a6749258b71095a029f6af58b9d
Victor Milhomem <victormllosada@gmail.com>
2024-07-17 16:51:31 -0300

    fixed trainer

--- experiment_config/no_domain_adaptation_config.py
+++ experiment_config/no_domain_adaptation_config.py
@@ -14,12 +14,18 @@ NO_DOMAIN_ADAPTATION_GLOBAL_PARAMS = {
     'num_runs': 5,
     'batch_size': 2,
     'val_fraction': 0.1,
-    'num_images_train': 10000,
+    'num_images_train': 2808,
     'rotate': True,
     'flip': True,
     'max_epochs': 25,
     'patience': 10,
     'progress_threshold': 0.01,
-    'lr_config': {'learning_rate': 0.001},
+    'lr_config': {
+        'name': 'exp',  
+        'lr0': 0.001,  
+        'warmup': 0.1,  
+        'alpha': 0.01,  
+        'beta': 0.5   
+    },
     'optimizer_config': {'name': 'adam'}
 }
