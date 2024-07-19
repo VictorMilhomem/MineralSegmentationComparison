a329b2bd731f26ca572acdb43d6a019cd0d11d47
Victor Milhomem <victormllosada@gmail.com>
2024-07-18 21:14:46 -0300

    another test

--- custom_trainer.py
+++ custom_trainer.py
@@ -821,11 +821,11 @@ class Trainer():
 
 
                 x_train = batch_images[ :, :, :, : self.channels]
-                y_train = batch_images[ :, :, :, self.channels : self.channels+1]
+                y_train = batch_images[ :, :, :, self.channels:]
                 x_train = np.repeat(batch_images[:, :, :, :self.channels], 3, axis=-1)
                 
 
-                #y_train = y_train[..., :self.model.output_shape[-1]]
+                y_train = y_train[..., :self.model.output_shape[-1]]
 
                 loss_train, y_pred = self._training_step(
                     model = self.model,
